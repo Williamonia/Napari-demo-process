136b2abeaaec1fbc8cdcd395be1c8be328b8b699
Chukwuebuka Okafornta <71377138+Williamonia@users.noreply.github.com>
2022-10-21 12:16:27 +0200

    Updated napari template for custom image processing (demo)

--- src/napari_demo_process/_widget.py
+++ src/napari_demo_process/_widget.py
@@ -42,5 +42,18 @@ def example_magic_widget(img_layer: "napari.layers.Image"):
 # Uses the `autogenerate: true` flag in the plugin manifest
 # to indicate it should be wrapped as a magicgui to autogenerate
 # a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+
+from napari.types import ImageData, LayerDataTuple
+def segment_image2(image: ImageData) -> LayerDataTuple:
+    """Apply thresholding and connected component analysis"""
+    from skimage.filters import threshold_otsu
+    from skimage.measure import label
+    
+    binary = image > threshold_otsu(image)
+    label_image = label(binary)
+    
+    output_tuple = (label_image, # first parameter of the tuple: data
+                    {'name': 'Output Label Image', 'opacity': 0.3}, # second parameter of the tuple: layer properties
+                    'labels') # third parameter of the tuple: layer type
+    
+    return output_tuple
